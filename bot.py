9d309c2a4905e8b386642d6d9887787431dc782b
Christopher Luu <christopherluu.dev@gmail.com>
2020-09-13 22:08:48 -0700

    added FINNHUB API

--- bot.py
+++ bot.py
@@ -1,15 +1,41 @@
 import os
-import discord
-
+import requests
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+FINNHUB = os.getenv('FINNHUB_API')
 
-client = discord.Client()
 
-@client.event
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user.name} has connected to Discord!')
+    
+@bot.command(name='tick')
+async def check_tick(ctx, arg):
+    url = 'https://finnhub.io/api/v1/quote?symbol=%s&token=%s' % (arg, FINNHUB)
+    url_info = 'https://finnhub.io/api/v1/stock/profile2?symbol=%s&token=%s' % (arg, FINNHUB)
 
-client.run(TOKEN)
+    r = requests.get(url)
+    r_info = requests.get(url_info)
+    
+    print(r.json())
+    print(r_info.json())
+    
+    response = r.json()
+    response_info = r_info.json()
+
+    company_name = response_info['name']
+    company_url = response_info['weburl']
+    current = response['c']
+    opening = response['o']
+    closing = response['pc']
+    high = response['h']
+    low = response['l']
+    
+    print(company_name, company_url, current, opening, closing, high, low)
+
+bot.run(TOKEN)
